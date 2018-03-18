8339f7f562dbad01ac31db99e898c29c255a4f7f
Bartek Zawadka <kontakt@bartoszzawadka.pl>
2018-03-18 21:32:48 +0100

    Project created

---
+++ Main/CustomWindowsManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using Caliburn.Micro;
+using MahApps.Metro.Controls;
+using Stroller.ViewModels.Common;
+
+namespace Stroller.Main
+{
+    public class CustomWindowsManager : WindowManager
+    {
+        readonly Dictionary<object, MetroWindow> _modelToWindows = new Dictionary<object, MetroWindow>();
+
+        protected override Window EnsureWindow(object model, object view, bool isDialog)
+        {
+            var window = base.EnsureWindow(model, view, isDialog);
+            AddWindow((MetroWindow) window, model);
+            return window;
+        }
+
+        public MetroWindow GetWindowForModel(ScreenBase model)
+        {
+            if (model != null && _modelToWindows.ContainsKey(model))
+                return _modelToWindows[model];
+            return null;
+        }
+
+        public void RemoveWindow(ScreenBase model)
+        {
+            _modelToWindows.Remove(model);
+        }
+
+        private void AddWindow(MetroWindow window, object model)
+        {
+            if (!_modelToWindows.ContainsKey(model))
+            {
+                _modelToWindows.Add(model, window);
+            }
+            else
+            {
+                _modelToWindows[model] = window;
+            }
+        }
+    }
+}
---
+++ ViewModels/Common/ScreenBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using Stroller.Main;
+
+namespace Stroller.ViewModels.Common
+{
+    public abstract class ScreenBase : Conductor<object>
+    {
+        protected static CustomWindowsManager WindowManager => IoC.Get<IWindowManager>() as CustomWindowsManager;
+
+        public ScreenBase ParentScreen { get; set; }
+
+        public override void TryClose(bool? dialogResult = null)
+        {
+            base.TryClose(dialogResult);
+            WindowManager.RemoveWindow(this);
+        }
+
+        public Task<MessageDialogResult> ShowMessage(string title, string message, bool showOnParentScreen = false)
+        {
+            var view = GetWindowThisOrParent(showOnParentScreen);
+            return view.ShowMessageAsync(title, message);
+        }
+
+        public Task<MessageDialogResult> ShowConfirmation(string title, string message,
+            string affirmativeButtonText = "Tak", string negativeButtonText = "Nie", bool showOnParentScreen = false)
+        {
+            var view = GetWindowThisOrParent(showOnParentScreen);
+            return view.ShowMessageAsync(title, message, MessageDialogStyle.AffirmativeAndNegative,
+                new MetroDialogSettings
+                {
+                    AffirmativeButtonText = affirmativeButtonText,
+                    NegativeButtonText = negativeButtonText,
+                    DefaultButtonFocus = MessageDialogResult.Affirmative
+                });
+        }
+
+        public bool? ShowDialog(ScreenBase viewModel)
+        {
+            return WindowManager.ShowDialog(viewModel);
+        }
+
+        public Task<ProgressDialogController> ShowProgress(string title, string message, bool showOnParentScreen = false)
+        {
+            var view = GetWindowThisOrParent(showOnParentScreen);
+            return view.ShowProgressAsync(title, message);
+        }
+
+        public virtual void SelectedContextItemDoubleClick(object context)
+        {
+
+        }
+
+        private MetroWindow GetWindowThisOrParent(bool useParent = false)
+        {
+            var screen = this;
+            if (useParent)
+                screen = ParentScreen;
+            var view = WindowManager.GetWindowForModel(screen);
+
+            if (view == null)
+                throw new Exception("Nie odnaleziono okna dla wybranego modelu");
+
+            return view;
+        }
+    }
+}
